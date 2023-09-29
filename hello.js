2f3325b9ed21c752495b8db142367b6b8b519bf1
Md Ratul Anjum <136952957+ratul4603@users.noreply.github.com>
2023-09-30 00:03:48 +0600

    Update hello.js

--- hello.js
+++ hello.js
@@ -5,7 +5,7 @@ const result = document.querySelector(".result");
 
 form.addEventListener("submit", (event)=>{
     event.preventDefault();
-    fetch(`https://restcountries.com/v3.1/name/${input.value}?fullText=true`)
+    fetch(`https://restcountries.com/v3.1/name/${input.value.tolowercase()}?fullText=true`)
     .then((data)=>data.json())
     .then((item)=>{
         result.innerHTML = `
@@ -13,7 +13,7 @@ form.addEventListener("submit", (event)=>{
             <h1>${input.value.toUpperCase()}</h1>
             <p><span>Official Name:</span> ${item[0].name.official}</p>
             <p><span>Capital:</span> ${item[0].capital[0]}</p>
-            <p><span>Continents:</span>${item[0].continents[0 ]} </p>
+            <p><span>Continents:</span> ${item[0].continents[0 ]} </p>
             <p><span>Population:</span> ${item[0].population}</p>
             <p><span>Currency:</span> ${item[0].currencies[Object.keys(item[0].currencies)[0]].name}, ${item[0].currencies[Object.keys(item[0].currencies)[0]].symbol} </p>
         `;
